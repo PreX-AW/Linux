dcd1ec04653d698a800c1f039e36062ad410a37c
wizard in MiLaptop ubuntu fossa <huwei040215@gmail.com>
2024-03-21 16:05:58 +0800

    rewrite my_stdio

--- file_io/myio/mystdio.c
+++ file_io/myio/mystdio.c
@@ -1,58 +1,101 @@
-#include"mystdio.h"
+#include "mystdio.h"
 #include <fcntl.h>
+#include <string.h>
 #include <sys/stat.h>
 
+// MY_FILE* my_fopen(const char* filepath, const char* flag)
+//{
+//     assert(filepath);
+//     int flags = 0;
+///
+//    umask(0);
+//    mode_t modecode = 0666;
+//    if (0 == strcmp(flag, "r"))
+//        flags |= O_RDONLY;
+//    if (0 == strcmp(flag, "w"))
+//        flags |= (O_WRONLY | O_CREAT | O_TRUNC);
+//    if (0 == strcmp(flag, "a"))
+//        flags |= O_APPEND;
+//    int fd = open(filepath, modecode);
+//    if (fd < 0) {
+//        return NULL;
+//    }
+//    MY_FILE* fp = (MY_FILE*)malloc(sizeof(MY_FILE));
+//    if (NULL == fp)
+//        return NULL;
+//    fp->fd = fd;
+//    fp->flag = flags;
+//    fp->count = 0;
+//    return fp;
+//}
+// size_t my_fwrite(const void* buffer, size_t size, size_t num, MY_FILE* stream)
+//{
+//    assert(buffer);
+//    size_t usr_size = size * num;
+//    size_t write_size = strlen((char*)stream->buffer) - stream->count;
+//    if (usr_size > write_size) {
+//        // my_fflush before
+//        memcpy(stream->buffer, buffer, use_size - write_size);
+//        stream->count = my_fflush(stream);
+//        memcpy(stream->buffer, buffer +, size_t n)
+//
+//    } else {
+//        // write dirctly
+//        memcpy(stream->buffer, buffer, write_size);
+//    }
+//}
+// int my_fclose(MY_FILE* stream)
+//{
+//    assert(stream);
+//    if (strlen((char*)stream->buffer) > 0)
+//        my_fflush(stream);
+//    stream->count = 0;
+//    free(stream);
+//    stream = NULL;
+//}
 
-
-
-MY_FILE*my_fopen(const char*filepath,const char*flag)
+MY_FILE* my_fopen(const char* pathname, const char* mode)
 {
-  assert(filepath);
-  int flags=0;
+    assert(pathname);
+    int flag = 0;
+    if (0 == strcmp(mode, "r")) {
+        flag |= O_RDONLY;
+    }
+    if (0 == strcmp(mode, "w")) {
+        flag |= O_WRONLY | O_CREAT | O_TRUNC;
+    }
+    if (0 == strcmp(mode, "a")) {
+        flag |= O_WRONLY | O_APPEND | O_CREAT;
+    } else {
+        // other mode
+    }
+    int fd = 0;
+    if (flag & O_CREAT)
+        fd = open(pathname, flag);
+    if (fd < 0)
+        return NULL;
 
-  umask(0);
-  mode_t modecode=0666;
-  if(0==strcmp(flag,"r"))flags|=O_RDONLY;
-  if(0==strcmp(flag,"w"))flags|=(O_WRONLY|O_CREAT|O_TRUNC);
-  if(0==strcmp(flag,"a"))flags|=O_APPEND;
-  int fd=open(filepath, modecode);
-if(fd<0)
-{
-  return NULL;
+    MY_FILE* fp = (MY_FILE*)malloc(sizeof(MY_FILE));
+    if (NULL == fp) {
+        // For malloc failed
+        close(fd);
+        return NULL;
+    }
 
+    fp->fd = fd;
+    fp->flags |= BUFF_ALL;
+    memset(fp->stream_buff, '\0', sizeof(fp->stream_buff));
+    fp->current_position = 0;
+    return fp;
 }
- MY_FILE* fp=(MY_FILE*)malloc(sizeof(MY_FILE*)); 
- if(NULL==fp)return NULL;
- fp->fd=fd;
- fp->flag=flags;
- fp->count=0;
- return fp;
-
-}
-size_t my_fwrite(const void*buffer,size_t size,size_t num,MY_FILE*stream)
+size_t my_fwrite(const void* ptr, size_t size, size_t num, MY_FILE* stream)
 {
-  assert(buffer);
-  size_t usr_size=size*num;
-  size_t write_size=strlen((char*)stream->buffer)-stream->count;
-  if(usr_size>write_size)
-  {
-      //my_fflush before
-      
-  }
-  else
-  {
-      //write dirctly
-  }
 }
-int my_fclose(MY_FILE*stream)
+
+int my_fclose(MY_FILE* stream)
 {
-  assert(stream);
-  if(strlen((char*)stream->buffer)>0)my_fflush(stream);
-  stream->count=0;
-  free(stream);
-  stream=NULL;
+}
 
-
-
-
+int my_fflush(MY_FILE* stream)
+{
 }
--- file_io/myio/mystdio.h
+++ file_io/myio/mystdio.h
@@ -1,29 +1,40 @@
-#include<stdio.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<assert.h>
-#include<string.h>
-#include<stdio.h>
-#define NUM 1024
-#define FLAG_READ   0x1
-#define FLAG_WRITE  0x2
-#define FLAG_APPEND 0x4
+#include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+// #define FLAG_READ 0x1
+// #define FLAG_WRITE 0x2
+// #define FLAG_APPEND 0x4
+// typedef struct MY_FILE {
+//     int fd;
+//     char* buffer[NUM];
+//     int flag;
+//     int count;
+//
+// } MY_FILE;
+//
+// MY_FILE* my_fopen(const char* filepath, const char* flag);
+// int my_fclose(MY_FILE* stream);
+// size_t my_fwrite(const void* buffer, size_t size, size_t num, MY_FILE* stream);
+#define BUF_LEN 1024
+// define buffer flush mode
+#define BUFF_NON 0x1
+#define BUFF_LINE 0x2
+#define BUFF_ALL 0x4
 
+// define MY_FILE structure
+typedef struct MY_FILE {
+    int fd;
+    int flags;
+    char stream_buff[BUF_LEN];
+    int current_position;
+} MY_FILE;
 
-
-typedef struct MY_FILE
-{
-  int fd;
-  char*buffer[NUM];
-  int flag;
-  int count;
-
-}MY_FILE;
-
-
-MY_FILE*my_fopen(const char*filepath,const char*flag);
-int my_fclose(MY_FILE*stream);
-size_t my_fwrite(const void *buffer,size_t size,size_t num,MY_FILE*stream);
-int my_fflush(MY_FILE*stream);
+MY_FILE* my_fopen(const char* pathname, const char* mode);
+size_t my_fwrite(const void* ptr, size_t size, size_t num, MY_FILE* stream);
+int my_fclose(MY_FILE* stream);
+int my_fflush(MY_FILE* stream);
