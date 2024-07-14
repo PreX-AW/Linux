27a8f54a98ad0e07b21a25cd3717629e1f4ea54d
wizard in MiLaptop ubuntu fossa <huwei040215@gmail.com>
2024-07-15 02:31:08 +0800

    udpate pipe/namedPipe

--- pipe/namedpipe/client.cc
+++ pipe/namedpipe/client.cc
@@ -1,5 +1,31 @@
 #include "component.hpp"
-int main()
-{
-    return 0;
+#include <assert.h>
+#include <cstdio>
+#include <cstring>
+#include <strings.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main() {
+  int wfd = open(pipeName.c_str(), O_WRONLY);
+  if (wfd < 0) {
+    std::cout << errno << ":" << strerror(errno) << std::endl;
+    exit(-1);
+  }
+  char buffer[NUM] = {0};
+  while (true) {
+    std::cout << "Input message:>" << std::endl;
+    char *msg = fgets(buffer, sizeof(buffer), stdin);
+    assert(msg);
+    (void)msg;
+    buffer[strlen(buffer) - 1] = 0;
+    if (0 == strcasecmp(buffer, "quit")) {
+      std::cout << "Client quit" << std::endl;
+      break;
+    }
+    ssize_t n = write(wfd, buffer, strlen(buffer));
+    assert(n >= 0);
+    (void)n;
+  }
+  close(wfd);
+  return 0;
 }
--- pipe/namedpipe/server.cc
+++ pipe/namedpipe/server.cc
@@ -1,11 +1,36 @@
 #include "component.hpp"
-#include <sys/stat.h>
+#include <cerrno>
+#include <cstring>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
 int main() {
   umask(0);
   int n = mkfifo(pipeName.c_str(), namedPipeMode);
-  if (0 == n) {
-
-  } else {
+  if (0 != n) {
+    std::cout << errno << strerror(errno) << std::endl;
   }
+  int rfd = open(pipeName.c_str(), O_RDONLY);
+  if (rfd < 0) {
+    std::cout << errno << ":" << strerror(errno) << std::endl;
+    exit(-1);
+  }
+  char buffer[NUM] = {0};
+  while (true) {
+    buffer[0] = 0;
+    ssize_t n = read(rfd, buffer, sizeof(buffer) - 1);
+    if (n > 0) {
+      buffer[n] = 0;
+      std::cout << buffer << std::endl;
+    } else if (0 == n) {
+      std::cout << "Client quit,server too" << std::endl;
+      break;
+    } else {
+      std::cout << errno << ":" << strerror(errno) << std::endl;
+      exit(-1);
+    }
+  }
+  close(rfd);
+  unlink(pipeName.c_str());
   return 0;
 }
