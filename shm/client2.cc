16a819e8fd38bd91e3361af06ab780ec55e45775
wizard in MiLaptop ubuntu fossa <huwei040215@gmail.com>
2024-08-01 18:21:14 +0800

    udpate shm/client2.cc

--- shm/client2.cc
+++ shm/client2.cc
@@ -1,32 +1,33 @@
 #include "common.hpp"
 shmid_ds buf;
 
-int main() {
-  //  key_t k = getKey();
-  //  // int shmId = shmget(k, SHM_SIZE, IPC_CREAT);
-  //  int shmId = getShm(k, SHM_SIZE);
-  //  if (-1 == shmId) {
-  //    std::cerr << errno << ":" << strerror(errno) << std::endl;
-  //    exit(3);
-  //  }
-  //  std::cout << "Key"
-  //            << ":" << dec2hex(k) << std::endl;
-  //  std::cout << "shmId"
-  //            << ":" << dec2hex(shmId) << std::endl;
-  //  shmctl(shmId, 2, &buf);
-  //  std::cout << dec2hex(buf.shm_perm.__key) << std::endl;
-  //  std::cout << buf.shm_cpid << " " << getpid() << std::endl;
-  //  sleep(10);
-  //  delShm(shmId);
-  //  std::cout << "Client distory shared memory." << std::endl;
+int main()
+{
+    //  key_t k = getKey();
+    //  // int shmId = shmget(k, SHM_SIZE, IPC_CREAT);
+    //  int shmId = getShm(k, SHM_SIZE);
+    //  if (-1 == shmId) {
+    //    std::cerr << errno << ":" << strerror(errno) << std::endl;
+    //    exit(3);
+    //  }
+    //  std::cout << "Key"
+    //            << ":" << dec2hex(k) << std::endl;
+    //  std::cout << "shmId"
+    //            << ":" << dec2hex(shmId) << std::endl;
+    //  shmctl(shmId, 2, &buf);
+    //  std::cout << dec2hex(buf.shm_perm.__key) << std::endl;
+    //  std::cout << buf.shm_cpid << " " << getpid() << std::endl;
+    //  sleep(10);
+    //  delShm(shmId);
+    //  std::cout << "Client distory shared memory." << std::endl;
 
-  shmClient client;
-  char *start = client.clientAddr();
-  char c = 'Z';
-  while (c >= 'A') {
-    start['Z' - c] = c;
-    c--;
-    sleep(1);
-  }
-  return 0;
+    shmClient client;
+    char* start = client.clientAddr();
+    char c = 'Z';
+    while (c >= 'A') {
+        start['Z' - c] = c;
+        c--;
+        sleep(1);
+    }
+    return 0;
 }
